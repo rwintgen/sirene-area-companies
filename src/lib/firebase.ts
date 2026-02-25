0d5b0795aebb6d46805e40dc1c1dca754bcfb78d
romain.wintgens <romain.wintgens@aispeakapp.com>
2026-02-25 16:45:19 +0000

    Added Firebase setup (untested)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,13 @@
-import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBM-SlSxzLSO40Tk45Y1KkPPyWG3sJQHWo",
+  authDomain: "test-firebase-ed0a6.firebaseapp.com",
+  projectId: "test-firebase-ed0a6",
+  storageBucket: "test-firebase-ed0a6.appspot.com",
+  messagingSenderId: "590276637809",
+  appId: "1:590276637809:web:154f2046331cce9f2617d0"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
